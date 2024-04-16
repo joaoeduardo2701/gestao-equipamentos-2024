3852835344cccedf67e52f2098eb1abcbb8a5411
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-04-16 16:19:38 -0300

    Menu e classe equipamentos criados

---
+++ src/GestaoEquipamentos.ConsoleApp/Equipamentos.cs
@@ -0,0 +1,20 @@
+﻿namespace GestaoEquipamentos.ConsoleApp;
+internal class Equipamentos
+{
+    public string Nome;
+    public double PrecoAquisicao;
+    public string NumeroSerie;
+    public string DataFabricacao;
+    public string Fabricante;
+
+    public Equipamentos(string nome, double precoAquisicao, string numeroSerie, string dataFabricacao, string fabricante)
+    {
+        Nome = nome;
+        PrecoAquisicao = precoAquisicao;
+        NumeroSerie = numeroSerie;
+        DataFabricacao = dataFabricacao;
+        Fabricante = fabricante;
+    }
+
+
+}
--- src/GestaoEquipamentos.ConsoleApp/Program.cs
+++ src/GestaoEquipamentos.ConsoleApp/Program.cs
@@ -1,10 +1,113 @@
-﻿namespace GestaoEquipamentos.ConsoleApp
+﻿namespace GestaoEquipamentos.ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        char opcao = 'n';
+        double precoAquisicao;
+        string nome, numeroSerie, dataFabricacao, fabricante;
+
+        do
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.Clear();
+            Console.WriteLine("-- Gestão de equipamentos --");
+            Console.WriteLine();
+            Console.WriteLine("1 - Cadastro de equipamentos ");
+            Console.WriteLine("2 - Controle de chamados ");
+            Console.WriteLine("3 - Sair");
+            Console.Write("-> ");
+
+            opcao = char.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            switch (opcao)
+            {
+                case '1':
+                    char opcaoCadastro;
+                    Console.WriteLine("-- Cadastro de equipamentos --");
+                    Console.WriteLine();
+
+                    Console.WriteLine("1 - Inserir um novo equipamentos");
+                    Console.WriteLine("2 - Visualizar equipamentos");
+                    Console.WriteLine("3 - Editar equipamentos");
+                    Console.WriteLine("4 - Excluir equipamentos");
+                    Console.WriteLine("5 - Sair");
+                    Console.Write("-> ");
+
+                    opcaoCadastro = char.Parse(Console.ReadLine());
+
+                    switch (opcaoCadastro)
+                    {
+                        case '1':
+                            Console.WriteLine("-- Inserir um novo equipamentos --");
+                            Console.WriteLine();
+
+                            Console.Write("Nome do equipamento: ");
+                            nome = Console.ReadLine();
+
+                            Console.Write("Preço de aquisição: ");
+                            precoAquisicao = double.Parse(Console.ReadLine());
+
+                            Console.Write("Número de série: ");
+                            numeroSerie = Console.ReadLine();
+
+                            Console.Write("Data de fabricação: ");
+                            dataFabricacao = Console.ReadLine();
+
+                            Console.Write("Fabricante: ");
+                            fabricante = Console.ReadLine();
+
+                            Equipamentos equipamento = new Equipamentos(nome, precoAquisicao, numeroSerie, dataFabricacao, fabricante);
+
+                            break;
+                        case '2':
+                            Console.WriteLine("-- Visualizar equipamentos --");
+                            Console.WriteLine();
+
+                            break;
+                        case '3':
+                            Console.WriteLine("-- Editar equipamentos --");
+                            Console.WriteLine();
+
+                            break;
+                        case '4':
+                            Console.WriteLine("-- Excluir equipamentos --");
+                            Console.WriteLine();
+
+                            break;
+                        case '5':
+                            Console.WriteLine("-- Inserir um novo equipamento --");
+                            Console.WriteLine();
+
+                            break;
+                        default:
+                            MensagemOpcaoInvalida();
+                            break;
+                    }
+                    break;
+                case '2':
+                    Console.WriteLine("-- Controle de chamados --");
+                    Console.WriteLine();
+                    break;
+                case '3':
+                    break;
+                default:
+                    MensagemOpcaoInvalida();
+                    break;
+            }
+
+            Console.WriteLine("Digite enter para continuar...");
+            Console.ReadKey();
+
+        } while (opcao != '3');
+    }
+
+    private static void MensagemOpcaoInvalida()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Por favor insira uma opção válida!!!");
+        Console.ForegroundColor = ConsoleColor.Gray;
     }
 }
